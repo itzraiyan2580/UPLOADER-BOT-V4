12d48abd2f33a00c5adcbceab21e5b7c9997f08d
itzraiyan2580 <112202419+itzraiyan2580@users.noreply.github.com>
2024-08-09 21:32:41 +0600

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,11 +11,11 @@ logging.basicConfig(
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7375820903:AAHXy37fvuQvZnHm5lg6W7bzoWjcbAOISIg")
     
-    API_ID = int(os.environ.get("API_ID", ""))
+    API_ID = int(os.environ.get("API_ID", "26684254"))
     
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "fc836096a68be3a4fcd7594cb3d9326f")
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     
@@ -39,11 +39,11 @@ class Config(object):
     
     DEF_WATER_MARK_FILE = "UploadLinkToFileBot"
     
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://renamebot:amrenamebot@cluster0.5ornz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
-    SESSION_NAME = os.environ.get("SESSION_NAME", "UploadLinkToFileBot")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "Cluster0")
     
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002235555810"))
     
     LOGGER = logging
 
